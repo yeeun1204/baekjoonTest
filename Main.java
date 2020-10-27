efedc1c5db4d0fc10b9cf68826ae2afad56c8ebd
yeeun1204 <61570702+yeeun1204@users.noreply.github.com>
2020-10-27 12:26:41 +0900

    백준알고리즘1

---
+++ Main.java
@@ -0,0 +1,23 @@
+package kh.project1.test1;
+
+import java.util.Scanner;
+
+
+public class Main
+{
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		
+		int i = scan.nextInt();
+		
+		for(int a=i;a>0;a--) {
+			for(int b=a;b<i;b++) {
+				System.out.print(" ");
+			}
+			for(int c=a;c>0;c--) {
+				System.out.print("*");
+			}
+			System.out.println( );
+		}
+	}
+}
