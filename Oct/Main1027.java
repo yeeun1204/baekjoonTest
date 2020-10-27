1bb43250233423a3030054d5e45d8544590d3b9f
yeeun1204 <61570702+yeeun1204@users.noreply.github.com>
2020-10-27 18:32:35 +0900

    별 찍기-6 25m

---
+++ Oct/Main1027.java
@@ -0,0 +1,41 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Main1027 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+//		int line = sc.nextInt();
+//		
+//		for(int a=1;a<=line;a++) {
+//			for(int sp=line;sp>=a+1;sp--) {
+//				System.out.print(" ");
+//			}
+//			
+//			for(int s=1;s <=(2*a-1);s++) {
+//				System.out.print("*");
+//			}
+//				
+//			System.out.println();
+//		}
+		
+		int line = sc.nextInt();
+		
+		for(int a=line;a>=1;a--) {
+			
+			for(int sp=line;sp>=a+1;sp--) {
+				System.out.print(" ");
+			}
+			
+			for(int s=1;s <=(2*line-a);s++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+
+	}
+
+}
